a2ff7f82a9ee78981d38d2ce6de4a2cebae2fcf7
Thomas M <t.moesse@gmail.com>
2025-10-27 19:28:47 +1100

    Adding connector configuration for My SQL Database

--- src/test/java/com/example/hibernatedemo/HibernateDemoApplicationTests.java
+++ src/test/java/com/example/hibernatedemo/HibernateDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.hibernatedemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,4 @@ class HibernateDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
 }
