5f68f1af9a3885a86181d185c00aee39efd64bfb
Thomas M <t.moesse@gmail.com>
2025-10-31 16:08:41 +1100

    Adding Surname attribute in User

--- src/main/java/com/example/hibernatedemo/model/User.java
+++ src/main/java/com/example/hibernatedemo/model/User.java
@@ -15,14 +15,16 @@ public class User {
     private Long id;
 
     private String name;
+    private String surname;
     private String email;
 
     // Constructors
     public User() {
     }
 
-    public User(String name, String email) {
+    public User(String name, String surname, String email) {
         this.name = name;
+        this.surname = surname;
         this.email = email;
     }
 
@@ -31,12 +33,24 @@ public class User {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getEmail() {
--- src/test/java/com/example/hibernatedemo/HibernateDemoApplicationTests.java
+++ src/test/java/com/example/hibernatedemo/HibernateDemoApplicationTests.java
@@ -8,4 +8,5 @@ class HibernateDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
 }
